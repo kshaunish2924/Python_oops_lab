b955cc98da0f9bc9e611c1bac970415f7b481015
kshaunish2924 <naikkshaunish@gmail.com>
2024-09-23 10:03:52 +0530

    Update inverted_pyramid_of_numbers.py

--- lab3/inverted_pyramid_of_numbers.py
+++ lab3/inverted_pyramid_of_numbers.py
@@ -1,4 +1,5 @@
-for i in range(1,6):
-    for j in range(5,0,-1):
-        print(i ,end=" ")
+for i in range(5,0,-1):
+    print(" " *(5-i) ,end=" ")
+    for j in range(i):
+        print(5-i+1 ,end=" ")
     print(" ")
