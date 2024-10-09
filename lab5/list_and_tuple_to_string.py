b87de419937eef4bb2499c6ee9635a6e427b3fbd
kshaunish2924 <naikkshaunish@gmail.com>
2024-10-09 16:14:52 +0530

    Add files via upload

---
+++ lab5/list_and_tuple_to_string.py
@@ -0,0 +1,6 @@
+il=[1,2,3,4,5]
+it=(6,7,8,9,10)
+clatas=list(map(str, il))+list(map(str,it))
+print("List of integers: ",il)
+print("Tuple of integers: ",it)
+print("Combined the list and tuple as string rsults in the following: ",clatas)
