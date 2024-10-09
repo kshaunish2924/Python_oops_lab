7e9e025908f2ec7890e9ffc93ef5753b57e1cb93
kshaunish2924 <naikkshaunish@gmail.com>
2024-10-09 16:24:56 +0530

    Add files via upload

---
+++ lab5/new_list_of_specific_elements_from_tuple_and_string_to_integer.py
@@ -0,0 +1,8 @@
+it=(1,2,3,4,5)
+string="6"
+si=[0,2,4]
+nl=[it[i] for i in si]
+ci=int(string)
+print("Integer tuple: ",it)
+print("New list of integers with specific indices: ",nl)
+print("The string as an integer: ",ci)
