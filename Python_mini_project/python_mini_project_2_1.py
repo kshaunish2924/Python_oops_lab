528fe0122e2222629ea4685b5f1ea24d1cb39e57
kshaunish2924 <naikkshaunish@gmail.com>
2024-11-17 16:06:09 +0530

    Update python_mini_project_2_1.py

--- Python_mini_project/python_mini_project_2_1.py
+++ Python_mini_project/python_mini_project_2_1.py
@@ -7,7 +7,7 @@ def fetch_news(api_key, keyword, num_headlines=5):
     
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise HTTPError for bad responses
+        response.raise_for_status()  
         data = response.json()
 
         if data["status"] == "ok":
@@ -19,10 +19,10 @@ def fetch_news(api_key, keyword, num_headlines=5):
         messagebox.showerror("Error", f"An error occurred: {e}")
 
 def display_news(articles):
-    # Clear the output text box
+   
     output_text.delete(1.0, tk.END)
     
-    # Display articles in the text box
+   
     for i, article in enumerate(articles, start=1):
         title = article['title']
         description = article.get('description', 'No description available.')
@@ -39,27 +39,27 @@ def on_fetch_button_click():
         return
     fetch_news(api_key, keyword)
 
-# Replace 'your_api_key' with your actual NewsAPI key
+
 api_key = "e76d3417701d4eb29838fe3c61a7e379"  
 
-# Create the main window
+
 root = tk.Tk()
 root.title("News Fetcher")
 
-# Create and place the keyword label and entry box
+
 label_keyword = tk.Label(root, text="Enter a keyword:")
 label_keyword.pack(pady=10)
 
 entry_keyword = tk.Entry(root, width=50)
 entry_keyword.pack(pady=10)
 
-# Create and place the "Fetch News" button
+
 fetch_button = tk.Button(root, text="Fetch News", command=on_fetch_button_click)
 fetch_button.pack(pady=20)
 
-# Create a Text widget to display news results
+
 output_text = tk.Text(root, height=15, width=80)
 output_text.pack(pady=10)
 
-# Start the Tkinter event loop
+
 root.mainloop()
